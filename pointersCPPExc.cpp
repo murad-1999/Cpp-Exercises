af702f0f058c624a7810ba7aa8a640b5de918e6d
murad-1999 <slackenheim@gmail.com>
2026-06-14 10:09:40 +0300

    make a short msg and commit and push

---
+++ pointersCPPExc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ // https://pynative.com/cpp-pointers-exercises/
+int main (){
+/*   Excerise 1  
+ int value =100;
+    int * ptr = &value;
+
+    std::cout << "Value: " << *ptr << std::endl;
+     */
+
+/* Excerise 2
+int number = 10;
+int * num_ptr = &number;
+*num_ptr = 99;
+std::cout << "Number: " << *num_ptr << std::endl;
+ */
+
+return 0;
+}
